612e75d9241921bab8792fd76aa9a09f41b63fa2
Rafal <pawelec.rafal@gmail.com>
2020-03-02 21:43:42 +0100

    fixed wpisz

--- hays_offer_generator/settings.py
+++ hays_offer_generator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '3!rdar*hjvl^2)4h$y%ltj2rls37^i9uad*^gz=iurer!52e!b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- hays_offer_generator/urls.py
+++ hays_offer_generator/urls.py
@@ -23,7 +23,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('generator/', include('generator.urls')),
-
+    path('', include('generator.urls')),
 ]
 
 if settings.DEBUG is True:
